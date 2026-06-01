b834f49bb9fef43e52c9d8af8ef6319adc0f72ef
Azra Celik <i576409@fontysict.nl>
2026-06-01 23:45:39 +0200

    Added IQuestionRepository.cs.

---
+++ TrackAdvisor.MODELS/Interfaces/IQuestionRepository.cs
@@ -0,0 +1,16 @@
+﻿using TrackAdvisor.MODELS;
+
+namespace TrackAdvisor.MODELS.Interfaces
+{
+    public interface IQuestionRepository
+    {
+        // Save a new question to the database
+        bool Save(Question question);
+
+        // Get all questions by topic
+        List<Question> FindByTopicID(int topicId);
+
+        // Delete a question by ID
+        void Delete(int id);
+    }
+}
