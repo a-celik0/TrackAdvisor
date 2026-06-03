e2cd492068108f8599513538193a695d9f14e45d
Azra Celik <i576409@fontysict.nl>
2026-06-03 22:47:16 +0200

    Added FakeQuestionRepository. To better understand the code some personal comments were added.

---
+++ TrackAdvisor.Tests/FakeQuestionRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackAdvisor.MODELS;
+using TrackAdvisor.MODELS.Interfaces;
+
+namespace TrackAdvisor.Tests
+{
+    public class FakeQuestionRepository : IQuestionRepository
+    {
+        //notebook(new one)
+        private List<Question> _questions = new List<Question>();
+
+        //writing to notebook = save
+        public bool Save(Question question)
+        {
+            _questions.Add(question);
+            return true;
+        }
+
+        //Search in the notebook 
+        public List<Question> FindByTopicID(int topicId)
+        {
+            return _questions.Where(q => q.TopicID == topicId).ToList();
+        }
+
+        //erase from the notebook
+        public void Delete(int id)
+        {
+            var question = _questions.FirstOrDefault(q => q.QuestionID == id);
+            if (question != null)
+            {
+                _questions.Remove(question);
+            }
+        }
+    }
+}
