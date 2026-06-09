495cbec8eeac79f6cb9e06208846c6e164aab116
Azra Celik <i576409@fontysict.nl>
2026-06-09 17:57:33 +0200

    Added IAnswerRepository.

---
+++ TrackAdvisor.MODELS/Interfaces/IAnswerRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrackAdvisor.MODELS;
+
+namespace TrackAdvisor.MODELS.Interfaces
+{
+    public interface IAnswerRepository
+    {
+        bool Save(Answer answer);  
+        List<Answer> FindByQuestionId(int questionId);
+    }
+}
