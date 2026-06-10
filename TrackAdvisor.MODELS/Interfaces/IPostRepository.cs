9b2ef6d3497825c881141461a10ca9feeddad482
Azra Celik <i576409@fontysict.nl>
2026-06-10 20:12:08 +0200

    Added Interface PostRepository.

---
+++ TrackAdvisor.MODELS/Interfaces/IPostRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackAdvisor.MODELS.Interfaces
+{
+    public interface IPostRepository
+    {
+        bool Save(ExperiencePost post);
+        List<ExperiencePost> FindByTopicId(int topicId);
+         void Delete(int id);
+    }
+}
